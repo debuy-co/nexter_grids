a56356d291eddd5784706b10041a2bdfdd2252bd
Debuy <43849498+AppPlusx2@users.noreply.github.com>
2023-04-17 07:42:11 +0800

    Building the Overall Layout - Part 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,17 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="container">
-      <p>Prova</p>
-    </div>
+    <body className="container">
+      <div className="sidebar">Sidebar</div>
+      <header className="header">Header</header>
+      <div className="realtors">Realtors</div>
+      <section className="features">Features</section>
+      <div className="story__pictures">Story pictures</div>
+      <div className="story__content">Story content</div>
+      <section className="homes">Homes</section>
+      <section className="gallery">Gallery</section>
+      <footer className="footer">Footer</footer>
+    </body>
   );
 }
 
