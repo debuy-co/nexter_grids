7a824c2cf2e8019ee44b22502a3b985818f93545
Debuy <43849498+AppPlusx2@users.noreply.github.com>
2023-04-28 07:35:35 +0800

    Building the Gallery - Part 2

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,23 +8,29 @@ import gal_6 from "../assets/img/gal-6.jpeg";
 import gal_7 from "../assets/img/gal-7.jpeg";
 import gal_8 from "../assets/img/gal-8.jpeg";
 import gal_9 from "../assets/img/gal-9.jpeg";
+import gal_10 from "../assets/img/gal-10.jpeg";
+import gal_11 from "../assets/img/gal-11.jpeg";
+import gal_12 from "../assets/img/gal-12.jpeg";
+import gal_13 from "../assets/img/gal-13.jpeg";
+import gal_14 from "../assets/img/gal-14.jpeg";
 
 function Gallery() {
   return (
     <section className="gallery">
-      <img
-        src={gal_1}
-        alt="Gallery image 1"
-        className="gallery__img gallery__img--img1"
-      />
-      <img src={gal_2} alt="Gallery image 2" className="gallery__img" />
-      <img src={gal_3} alt="Gallery image 3" className="gallery__img" />
-      <img src={gal_4} alt="Gallery image 4" className="gallery__img" />
-      <img src={gal_5} alt="Gallery image 5" className="gallery__img" />
-      <img src={gal_6} alt="Gallery image 6" className="gallery__img" />
-      <img src={gal_7} alt="Gallery image 7" className="gallery__img" />
-      <img src={gal_8} alt="Gallery image 8" className="gallery__img" />
-      <img src={gal_9} alt="Gallery image 9" className="gallery__img" />
+      <img src={gal_1} alt="1" className="gallery__img gallery__img--img1" />
+      <img src={gal_2} alt="2" className="gallery__img gallery__img--img2" />
+      <img src={gal_3} alt="3" className="gallery__img gallery__img--img3" />
+      <img src={gal_4} alt="4" className="gallery__img gallery__img--img4" />
+      <img src={gal_5} alt="5" className="gallery__img gallery__img--img5" />
+      <img src={gal_6} alt="6" className="gallery__img gallery__img--img6" />
+      <img src={gal_7} alt="7" className="gallery__img gallery__img--img7" />
+      <img src={gal_8} alt="8" className="gallery__img gallery__img--img8" />
+      <img src={gal_9} alt="9" className="gallery__img gallery__img--img9" />
+      <img src={gal_10} alt="10" className="gallery__img gallery__img--img10" />
+      <img src={gal_11} alt="11" className="gallery__img gallery__img--img11" />
+      <img src={gal_12} alt="12" className="gallery__img gallery__img--img12" />
+      <img src={gal_13} alt="13" className="gallery__img gallery__img--img13" />
+      <img src={gal_14} alt="14" className="gallery__img gallery__img--img14" />
     </section>
   );
 }
